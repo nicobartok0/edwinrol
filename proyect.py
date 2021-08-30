b16ea056aa6e085e74be82f53de6ea24fe242bfe
nicobartok0 <nicobartok0@gmail.com>
2021-08-30 10:11:26 -0300

    Alpha 1.2

--- proyect.py
+++ proyect.py
@@ -23,7 +23,7 @@ class Aplicacion(ttk.Frame):
 
         self.root.geometry('800x600')
         self.root.resizable(width=False, height=False)
-        self.root.title('Jerarquía Table Manager Alpha 1.1')
+        self.root.title('Jerarquía Table Manager Alpha 1.3')
 
         photo = PhotoImage(file = "dado.png")
         self.root.iconphoto(False, photo)
@@ -154,11 +154,53 @@ class Aplicacion(ttk.Frame):
         #Bonificaciones de armadura
         tk.Label(self.root, text='Bonif.').place(x= 595, y=10)
         
-        bon1 = ttk.Combobox(self, state='readonly')
-        bon1.place(x=200,y=200)
-        bon1.configure(width=2, height=2)
-        bon1["values"] = [0, 10, 20, 30]
+        bon1 = ttk.Combobox(self.root, state='readonly')
+        bon1.place(x=595,y=60)
+        bon1.configure(width=4, height=4)
+        bon1["values"] = [0, 10, 20, 30, 40]
+        bon1.current(0)
         
+        bon2 = ttk.Combobox(self.root, state='readonly')
+        bon2.place(x=595,y=110)
+        bon2.configure(width=4, height=4)
+        bon2["values"] = [0, 10, 20, 30, 40]
+        bon2.current(0)
+
+        bon3 = ttk.Combobox(self.root, state='readonly')
+        bon3.place(x=595,y=160)
+        bon3.configure(width=4, height=4)
+        bon3["values"] = [0, 10, 20, 30, 40]
+        bon3.current(0)
+
+        bon4 = ttk.Combobox(self.root, state='readonly')
+        bon4.place(x=595,y=210)
+        bon4.configure(width=4, height=4)
+        bon4["values"] = [0, 10, 20, 30, 40]
+        bon4.current(0)
+
+        bon5 = ttk.Combobox(self.root, state='readonly')
+        bon5.place(x=595,y=260)
+        bon5.configure(width=4, height=4)
+        bon5["values"] = [0, 10, 20, 30, 40]
+        bon5.current(0)
+        
+        bon6 = ttk.Combobox(self.root, state='readonly')
+        bon6.place(x=595,y=310)
+        bon6.configure(width=4, height=4)
+        bon6["values"] = [0, 10, 20, 30, 40]
+        bon6.current(0)
+
+        bon7 = ttk.Combobox(self.root, state='readonly')
+        bon7.place(x=595,y=360)
+        bon7.configure(width=4, height=4)
+        bon7["values"] = [0, 10, 20, 30, 40]
+        bon7.current(0)
+
+        bon8 = ttk.Combobox(self.root, state='readonly')
+        bon8.place(x=595,y=410)
+        bon8.configure(width=4, height=4)
+        bon8["values"] = [0, 10, 20, 30, 40]
+        bon8.current(0)
 
         #Funciones de ATAQUE NORMAL
         def atk1():
@@ -169,6 +211,8 @@ class Aplicacion(ttk.Frame):
             if c1.get() == '' or ce1.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_atk_1 = (dado1 + dado2 + int(c1.get())) - int(ce1.get())
+            if int(bon1.get()) > 0:
+                res_atk_1 = res_atk_1 - res_atk_1 * (int(bon1.get())/100)
             print("El resultado de la tirada: ", res_atk_1)
             res1.config(text=res_atk_1)
             dado_text1.config(text=dado1)
@@ -183,6 +227,8 @@ class Aplicacion(ttk.Frame):
             if c2.get() == '' or ce2.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_atk_2 = (dado1 + dado2 + int(c2.get())) - int(ce2.get())
+            if int(bon2.get()) > 0:
+                res_atk_2 = res_atk_2 - res_atk_2 * (int(bon2.get())/100)
             print("El resultado de la tirada: ", res_atk_2)
             res2.config(text=res_atk_2)
             dado_text1.config(text=dado1)
@@ -197,6 +243,8 @@ class Aplicacion(ttk.Frame):
             if c3.get() == '' or ce3.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_atk_3 = (dado1 + dado2 + int(c3.get())) - int(ce3.get())
+            if int(bon3.get()) > 0:
+                res_atk_3 = res_atk_3 - res_atk_3 * (int(bon3.get())/100)
             print("El resultado de la tirada: ", res_atk_3)
             res3.config(text=res_atk_3)
             dado_text1.config(text=dado1)
@@ -211,6 +259,8 @@ class Aplicacion(ttk.Frame):
             if c4.get() == '' or ce4.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_atk_4 = (dado1 + dado2 + int(c4.get())) - int(ce4.get())
+            if int(bon4.get()) > 0:
+                res_atk_4 = res_atk_4 - res_atk_4 * (int(bon4.get())/100)
             print("El resultado de la tirada: ", res_atk_4)
             res4.config(text=res_atk_4)
             dado_text1.config(text=dado1)
@@ -225,6 +275,8 @@ class Aplicacion(ttk.Frame):
             if c5.get() == '' or ce5.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_atk_5 = (dado1 + dado2 + int(c5.get())) - int(ce5.get())
+            if int(bon5.get()) > 0:
+                res_atk_5 = res_atk_5 - res_atk_5 * (int(bon5.get())/100)
             print("El resultado de la tirada: ", res_atk_5)
             res5.config(text=res_atk_5)
             dado_text1.config(text=dado1)
@@ -239,6 +291,8 @@ class Aplicacion(ttk.Frame):
             if c6.get() == '' or ce6.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_atk_6 = (dado1 + dado2 + int(c6.get())) - int(ce6.get())
+            if int(bon6.get()) > 0:
+                res_atk_6 = res_atk_6 - res_atk_6 * (int(bon6.get())/100)
             print("El resultado de la tirada: ", res_atk_6)
             res6.config(text=res_atk_6)
             dado_text1.config(text=dado1)
@@ -253,6 +307,8 @@ class Aplicacion(ttk.Frame):
             if c7.get() == '' or ce7.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_atk_7 = (dado1 + dado2 + int(c7.get())) - int(ce7.get())
+            if int(bon7.get()) > 0:
+                res_atk_7 = res_atk_7 - res_atk_7 * (int(bon7.get())/100)
             print("El resultado de la tirada: ", res_atk_7)
             res7.config(text=res_atk_7)
             dado_text1.config(text=dado1)
@@ -267,6 +323,8 @@ class Aplicacion(ttk.Frame):
             if c8.get() == '' or ce8.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_atk_8 = (dado1 + dado2 + int(c8.get())) - int(ce8.get())
+            if int(bon8.get()) > 0:
+                res_atk_8 = res_atk_8 - res_atk_8 * (int(bon8.get())/100)
             print("El resultado de la tirada: ", res_atk_8)
             res8.config(text=res_atk_8)
             dado_text1.config(text=dado1)
@@ -281,6 +339,8 @@ class Aplicacion(ttk.Frame):
             if d1.get() == '' or ce1.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_def_1 = int(ce1.get()) - (int(d1.get()) + dado1)
+            if int(bon1.get()) > 0:
+                res_def_1 = res_def_1 - res_def_1 * (int(bon1.get())/100)
             print("El resultado de la tirada: ", res_def_1)
             res1.config(text=res_def_1)
             dado_text1.config(text=dado1)
@@ -293,6 +353,8 @@ class Aplicacion(ttk.Frame):
             if d2.get() == '' or ce2.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_def_2 = int(ce2.get()) - (int(d2.get()) + dado1)
+            if int(bon2.get()) > 0:
+                res_def_2 = res_def_2 - res_def_2 * (int(bon2.get())/100)
             print("El resultado de la tirada: ",  res_def_2)
             res2.config(text= res_def_2)
             dado_text1.config(text=dado1)
@@ -305,6 +367,8 @@ class Aplicacion(ttk.Frame):
             if d3.get() == '' or ce3.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_def_3 = int(ce3.get()) - (int(d3.get()) + dado1)
+            if int(bon3.get()) > 0:
+                res_def_3 = res_def_3 - res_def_3 * (int(bon3.get())/100)
             print("El resultado de la tirada: ", res_def_3)
             res3.config(text=res_def_3)
             dado_text1.config(text=dado1)
@@ -317,6 +381,8 @@ class Aplicacion(ttk.Frame):
             if d4.get() == '' or ce4.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_def_4 = int(ce4.get()) - (int(d4.get()) + dado1)
+            if int(bon1.get()) > 0:
+                res_def_4 = res_def_4 - res_def_4 * (int(bon4.get())/100)
             print("El resultado de la tirada: ", res_def_4)
             res4.config(text=res_def_4)
             dado_text1.config(text=dado1)
@@ -329,6 +395,8 @@ class Aplicacion(ttk.Frame):
             if d5.get() == '' or ce5.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_def_5 = int(ce5.get()) - (int(d5.get()) + dado1)
+            if int(bon5.get()) > 0:
+                res_def_5 = res_def_5 - res_def_5 * (int(bon5.get())/100)
             print("El resultado de la tirada: ", res_def_5)
             res5.config(text=res_def_5)
             dado_text1.config(text=dado1)
@@ -341,6 +409,8 @@ class Aplicacion(ttk.Frame):
             if d6.get() == '' or ce6.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_def_6 = int(ce6.get()) - (int(d6.get()) + dado1)
+            if int(bon6.get()) > 0:
+                res_def_6 = res_def_6 - res_def_6 * (int(bon6.get())/100)
             print("El resultado de la tirada: ", res_def_6)
             res6.config(text=res_def_6)
             dado_text1.config(text=dado1)
@@ -353,6 +423,8 @@ class Aplicacion(ttk.Frame):
             if d7.get() == '' or ce7.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_def_7 = int(ce7.get()) - (int(d7.get()) + dado1)
+            if int(bon7.get()) > 0:
+                res_def_7 = res_def_7 - res_def_7 * (int(bon7.get())/100)
             print("El resultado de la tirada: ", res_def_7)
             res7.config(text=res_def_7)
             dado_text1.config(text=dado1)
@@ -365,6 +437,8 @@ class Aplicacion(ttk.Frame):
             if d8.get() == '' or ce8.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_def_8 = int(ce8.get()) - (int(d8.get()) + dado1)
+            if int(bon8.get()) > 0:
+                res_def_8 = res_def_8 - res_def_8 * (int(bon8.get())/100)
             print("El resultado de la tirada: ", res_def_8)
             res8.config(text=res_def_8)
             dado_text1.config(text=dado1)
@@ -382,6 +456,8 @@ class Aplicacion(ttk.Frame):
             if c1.get() == '' or ce1.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_matk_1 = (dado1 + dado2 + dado3 + int(c1.get())) - int(ce1.get())
+            if int(bon1.get()) > 0:
+                res_matk_1 = res_matk_1 - res_matk_1 * (int(bon1.get())/100)
             
             print("El resultado de la tirada: ", res_matk_1)
             res1.config(text=res_matk_1)
@@ -399,6 +475,9 @@ class Aplicacion(ttk.Frame):
             if c2.get() == '' or ce2.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_matk_2= (dado1 + dado2 + dado3 + int(c2.get())) - int(ce2.get())
+            if int(bon2.get()) > 0:
+                res_matk_2 = res_matk_2 - res_matk_2 * (int(bon2.get())/100)
+            
             print("El resultado de la tirada: ", res_matk_2)
             res1.config(text=res_matk_2)
             dado_text1.config(text=dado1)
@@ -415,6 +494,9 @@ class Aplicacion(ttk.Frame):
             if c3.get() == '' or ce3.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_matk_3 = (dado1 + dado2 + dado3 + int(c3.get())) - int(ce3.get())
+            if int(bon3.get()) > 0:
+                res_matk_3 = res_matk_3 - res_matk_3 * (int(bon3.get())/100)
+            
             print("El resultado de la tirada: ", res_matk_3)
             res1.config(text=res_matk_3)
             dado_text1.config(text=dado1)
@@ -431,6 +513,9 @@ class Aplicacion(ttk.Frame):
             if c4.get() == '' or ce4.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_matk_4 = (dado1 + dado2 + dado3 + int(c4.get())) - int(ce4.get())
+            if int(bon4.get()) > 0:
+                res_matk_4 = res_matk_4 - res_matk_4 * (int(bon4.get())/100)
+            
             print("El resultado de la tirada: ", res_matk_4)
             res1.config(text=res_matk_4)
             dado_text1.config(text=dado1)
@@ -447,6 +532,9 @@ class Aplicacion(ttk.Frame):
             if c5.get() == '' or ce5.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_matk_5 = (dado1 + dado2 + dado3 + int(c5.get())) - int(ce5.get())
+            if int(bon5.get()) > 0:
+                res_matk_5 = res_matk_5 - res_matk_5 * (int(bon5.get())/100)
+            
             print("El resultado de la tirada: ", res_matk_5)
             res1.config(text=res_matk_5)
             dado_text1.config(text=dado1)
@@ -463,6 +551,9 @@ class Aplicacion(ttk.Frame):
             if c6.get() == '' or ce6.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_matk_6 = (dado1 + dado2 + dado3 + int(c6.get())) - int(ce6.get())
+            if int(bon6.get()) > 0:
+                res_matk_6 = res_matk_6 - res_matk_6 * (int(bon6.get())/100)
+            
             print("El resultado de la tirada: ", res_matk_6)
             res1.config(text=res_matk_6)
             dado_text1.config(text=dado1)
@@ -479,6 +570,9 @@ class Aplicacion(ttk.Frame):
             if c7.get() == '' or ce7.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_matk_7 = (dado1 + dado2 + dado3 + int(c7.get())) - int(ce7.get())
+            if int(bon7.get()) > 0:
+                res_matk_7 = res_matk_7 - res_matk_7 * (int(bon7.get())/100)
+            
             print("El resultado de la tirada: ", res_matk_7)
             res1.config(text=res_matk_7)
             dado_text1.config(text=dado1)
@@ -495,6 +589,9 @@ class Aplicacion(ttk.Frame):
             if c8.get() == '' or ce8.get() == '':
                 messagebox.showerror(message='Falta uno o más valores para ingresar.', title='Error de datos.')
             res_matk_8 = (dado1 + dado2 + dado3 + int(c8.get())) - int(ce8.get())
+            if int(bon8.get()) > 0:
+                res_matk_8 = res_matk_8 - res_matk_8 * (int(bon8.get())/100)
+            
             print("El resultado de la tirada: ", res_matk_8)
             res1.config(text=res_matk_8)
             dado_text1.config(text=dado1)
